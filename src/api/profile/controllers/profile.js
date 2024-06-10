f8c03ded245e4a65dbb2ef083e12c0443798c028
berkertoker <berkertoker2001@gmail.com>
2024-06-10 22:10:34 +0300

    first commit

---
+++ src/api/profile/controllers/profile.js
@@ -0,0 +1,53 @@
+
+'use strict';
+
+/**
+ * profile controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::profile.profile', ({strapi: Strapi}) =>({
+    async createMe(ctx){
+        try{
+            const user= ctx.state.user;
+            if(!user){
+                return ctx.badRequest({ messages: [{ id: 'No authorized user found!' }]});
+
+            }
+             const result = await  strapi.entityService.create('api::profile.profile',{
+                data: {
+                    fullName: ctx.request.body.fullName,
+                    email: user.email,
+                    user: user.id,
+                }
+             });
+             return result;
+        }catch(err){
+           return ctx.badRequest({messages: [{ id: 'Error'}]});
+        }
+    },
+    async getMe(ctx){
+        try{
+            const user= ctx.state.user;
+            if(!user){
+                return ctx.badRequest({messages: [{ id: 'No authorized user found!'}]});
+            }
+             const result = await strapi.db.query('api::profile.profile').findOne({
+                where:{
+                    user: {
+                        id:{
+                            $eq: user.id
+                        }
+                    }
+                },
+                populate: {
+                   image:true 
+                }
+             })
+             return result;
+        }catch(err){
+           return ctx.badRequest({messages: [{ id: 'Error'}]});
+        }
+    }  
+}));
